fbdb2003352ad440ac74c300bf3befbb28e94fb3
Adriano Fabio Brito <adriano.fabio@gmail.com>
2024-11-20 01:15:08 +0000

    /tmp/uploads

--- controllers/fileMetadataController.js
+++ controllers/fileMetadataController.js
@@ -1,20 +1,19 @@
-
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: '/tmp/uploads/' });
 
 exports.uploadFile = [
-    upload.single('upfile'), // 'upfile' é o nome do campo do arquivo no formulário
-    (req, res) => {
-        if (!req.file) {
-            return res.status(400).json({ error: 'No file uploaded' });
-        }
-        
-        const fileMetadata = {
-            name: req.file.originalname,
-            type: req.file.mimetype,
-            size: req.file.size
-        };
-        
-        res.json(fileMetadata);
+  upload.single('upfile'), // 'upfile' é o nome do campo do arquivo no formulário
+  (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
     }
+
+    const fileMetadata = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    };
+
+    res.json(fileMetadata);
+  },
 ];
